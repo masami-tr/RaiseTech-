d210328d1b6b99f836af6d3e0fc3268ffc0f10c7
masami-tr <m3o.mmmnnn@gmail.com>
2023-09-22 19:14:58 +0900

    プロジェクトセットアップ

---
+++ src/main/java/com/raisetech9/workoutmanagement/WorkoutmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech9.workoutmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech9/workoutmanagement/WorkoutmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech9.workoutmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
